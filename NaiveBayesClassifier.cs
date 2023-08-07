6e647d74ec42c59e11ed955f7cf819cc3f2ba4dd
Krieo <krieolin@gmail.com>
2023-08-07 11:50:51 +0200

    added a basic test case for naive bayes

---
+++ NaiveBayesClassifier.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _219003234_Naidoo_KN_AAI
+{
+    public class NaiveBayesClassifier
+    {
+        private Dictionary<string, Dictionary<string, List<double>>> featuresByClass;
+        private Dictionary<string, int> classCounts;
+
+        public NaiveBayesClassifier()
+        {
+            featuresByClass = new Dictionary<string, Dictionary<string, List<double>>>();
+            classCounts = new Dictionary<string, int>();
+        }
+
+        public void Train(List<DataRecord> trainingData)
+        {
+            foreach (var record in trainingData)
+            {
+                string className = record.MachineFailure == 1 ? "1" : "0"; // 1 = Failure, 0 = No Failure
+
+                if (!featuresByClass.ContainsKey(className))
+                {
+                    featuresByClass[className] = new Dictionary<string, List<double>>();
+                    classCounts[className] = 0;
+                }
+
+                foreach (var property in typeof(DataRecord).GetProperties())
+                {
+                    if (property.PropertyType == typeof(double) || property.PropertyType == typeof(int))
+                    {
+                        string propertyName = property.Name;
+                        double value = Convert.ToDouble(property.GetValue(record));
+
+                        if (!featuresByClass[className].ContainsKey(propertyName))
+                        {
+                            featuresByClass[className][propertyName] = new List<double>();
+                        }
+
+                        featuresByClass[className][propertyName].Add(value);
+                    }
+                }
+
+                classCounts[className]++;
+            }
+        }
+
+        public int Predict(DataRecord testRecord)
+        {
+            double probFailure = CalculateClassProbability("1", testRecord);
+            double probNoFailure = CalculateClassProbability("0", testRecord);
+
+            if (probFailure > probNoFailure)
+            {
+                return 1; // Predicted Failure
+            }
+            else
+            {
+                return 0; // Predicted No Failure
+            }
+        }
+
+        private double CalculateClassProbability(string className, DataRecord testRecord)
+        {
+            double classProbability = (double)classCounts[className] / classCounts.Values.Sum();
+            double featureProbability = 1.0;
+
+            foreach (var property in typeof(DataRecord).GetProperties())
+            {
+                if (property.PropertyType == typeof(double) || property.PropertyType == typeof(int))
+                {
+                    string propertyName = property.Name;
+                    double value = Convert.ToDouble(property.GetValue(testRecord));
+
+                    if (featuresByClass[className].ContainsKey(propertyName))
+                    {
+                        double mean = featuresByClass[className][propertyName].Average();
+                        double stdDev = Math.Sqrt(featuresByClass[className][propertyName].Select(x => Math.Pow(x - mean, 2)).Sum() / featuresByClass[className][propertyName].Count);
+
+                        double probability = (1.0 / (stdDev * Math.Sqrt(2 * Math.PI))) * Math.Exp(-Math.Pow(value - mean, 2) / (2 * Math.Pow(stdDev, 2)));
+                        featureProbability *= probability;
+                    }
+                }
+            }
+
+            return classProbability * featureProbability;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿using _219003234_Naidoo_KN_AAI;
+﻿using System;
+using System.Collections.Generic;
 
-class Program
+namespace _219003234_Naidoo_KN_AAI
 {
-    static void Main(string[] args)
+    class Program
     {
-        FileHandler fileHandler = new FileHandler();
-        fileHandler.readFromFile();
+        static void Main(string[] args)
+        {
+            FileHandler fileHandler = new FileHandler();
+            List<DataRecord> trainingData = fileHandler.readFromFile();
+
+            // Initialize and train the NaiveBayesClassifier
+            NaiveBayesClassifier classifier = new NaiveBayesClassifier();
+            classifier.Train(trainingData);
+
+            // Create a test record
+            DataRecord testRecord = new DataRecord
+            {
+                Id = 123, // Assign appropriate values for each feature
+                ProductId = "ABC123",
+                Type = "L",
+                AirTemperature = 301.5,
+                ProcessTemperature = 310.8,
+                RotationalSpeed = 1408,
+                Torque = 43.6,
+                ToolWear = 166,
+                MachineFailure = 0, // This value is not used for prediction
+                TWF = 0,
+                HDF = 0,
+                PWF = 0,
+                OSF = 0,
+                RNF = 0
+            };
+
+            // Predict machine failure
+            int predictedMachineFailure = classifier.Predict(testRecord);
+
+            Console.WriteLine($"Predicted Machine Failure: {predictedMachineFailure}");
+        }
     }
 }
