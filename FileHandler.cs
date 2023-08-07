4baa7a2149bc94466bdefb00b67e15fa3907210e
Krieo <krieolin@gmail.com>
2023-08-07 10:27:52 +0200

    Add project files.

---
+++ FileHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _219003234_Naidoo_KN_AAI
+{
+    public class FileHandler
+    {
+
+    }
+}
